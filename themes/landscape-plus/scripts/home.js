8619ed3e7d7fe428879c075bee75e7e281424003
libang <libang@giga.build>
2017-04-07 17:44:39 +0800

    add static page

---
+++ themes/landscape-plus/scripts/home.js
@@ -0,0 +1,20 @@
+
+hexo.extend.helper.register('home', function(page){
+  var categories = hexo.locals.get('categories').data;
+  var posts = {};
+  // for (var cat in categories) {
+  //   names.push(categories[cat].name);
+  // };
+  page.posts.each(function(post) {
+    post.categories.each(function(cat) {
+      if (posts[cat.name]) {
+        posts[cat.name].push(post);
+      } else {
+        posts[cat.name] = [];
+        posts[cat.name].push(post);
+      }
+    })
+  });
+
+  return posts;
+});
